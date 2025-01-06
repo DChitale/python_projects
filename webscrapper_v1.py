64b6cf991a080cd241a35b7fe7227de9fcf82424
DChitale <chitaledhananjay70@gmail.com>
2025-01-06 23:25:28 +0530

    Minor changes in v1

--- webscrapper_v1.py
+++ webscrapper_v1.py
@@ -5,10 +5,10 @@ def get_page(url):
    response = requests.get(url)
    soup =BeautifulSoup(response.content,'html.parser')
 
-   tag = soup.find_all('a')
+   tag = soup.find_all('h4')
    for t in tag:
-      url2 =t.get('href')
-      print(url2)
+
+      print(t)
   
 
 get_page(input("what url you want to scrape? "))
